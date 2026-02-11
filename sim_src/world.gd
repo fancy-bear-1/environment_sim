5fbad1793bd96e96d05a4b4107ae7a36c1e0f3ca
c-alexander.nielsen <c-alexander.nielsen@charter.com>
2026-02-11 16:28:30 -0700

    initial body of water update

--- sim_src/world.gd
+++ sim_src/world.gd
@@ -23,6 +23,9 @@ static var day_to_day_variance := 20
 static var world_scale := 1.0
 static var weathering_radius := 1
 
+static var body_of_water_threshold = 1.0
+
+
 var chunklist: Array[Array] = []
 var generated_biomelist: Array[Biome] = []
 var year: int = 0
@@ -38,7 +41,31 @@ var world_seed: int
 @onready var world:World=$World
 
 func generate_steepness(elevation_map):
-    
+
+    # initialize gradient map
+    var res = []
+    for x in range(width):
+        var tmp = []
+        for y in range(height):
+            tmp.append(0.0)
+        res.append(tmp)
+
+    # for every chunk on the map
+    for x in range(width):
+        for y in range(height):
+            var xm := max(x - 1, 0)
+            var xp := min(x + 1, width - 1)
+            var ym := max(y - 1, 0)
+            var yp := min(y + 1, height - 1)
+
+            var dx := (elevation_map[xp][y] - elevation_map[xm][y]) * 0.5
+            var dy := (elevation_map[x][yp] - elevation_map[x][ym]) * 0.5
+
+            res[x][y] = Vector2(dx, dy)
+
+    return res
+
+func elevation_second_derivative(steepness_map):
     # initialize gradient map
     var res = []
     for x in range(width):
@@ -55,13 +82,13 @@ func generate_steepness(elevation_map):
 			var ym := max(y - 1, 0)
 			var yp := min(y + 1, height - 1)
 
-			var dx := (data[xp][y] - data[xm][y]) * 0.5
-			var dy := (data[x][yp] - data[x][ym]) * 0.5
-
+			var dx := (steepness_map[xp][y].x - steepness_map[xm][y].x) * 0.5
+			var dy := (steepness_map[x][yp].y - steepness_map[x][ym].y) * 0.5
+            dy += (steepness_map[xp][y].y - steepness_map[xm][y].y) * 0.5
+			dx += (steepness_map[x][yp].x - steepness_map[x][ym].x) * 0.5
 			res[x][y] = Vector2(dx, dy)
     
     return res
-
 
 
 func generate_mountains():
@@ -115,30 +142,92 @@ func generate_mountains():
 
     return elevation_map
 
-func generate_water(elevation_map, steepness_map):
-    # first do a base layer of water at elevation 1 or less
 
-    # then
+func generate_water(elevation_map, steepness_map):
+    var body_of_water_map = []
+
+    var second_derivative = elevation_second_derivative(steepness_map)
+
+    for x in range(width):
+        var tmp:Array[float]
+        for y in range(height):
+            tmp.append(false)
+        body_of_water_map.append(tmp)
+
+    # first do a base layer of water at elevation 1 or less
+    for x in range(width):
+        for y in range(height):
+            if elevation_map[x][y] <= 1.0:
+                body_of_water_map[x][y] = true
+
+    for x in range(width):
+        for y in range(height):
+            # if the current point is a local minimum, set it to true on the map and fill out the rest of the body
+            if steepness_map[x][y].magnitude() <= body_of_water_threshold and second_derivative[x][y] > 0:
+                var tmp = fill_water_body(Vector2(x, y), x, y, elevation_map, steepness_map, second_derivative)
+                # to fill it out, find the borders of the body by finding the other points where steepness is below the threshold but the second derivative is pointing away from the center
+                for coord in tmp:
+                    body_of_water_map[coord.x][coord.y] = true
+
+
+func fill_water_body(center, x, y, elevation_map, steepness_map, second_derivative_map):
+    var direction_from_center = Vector2(x - center.x, y - center.y).normalize()
+    var res: Array[Vector2] = []
+    
+    # if the current chunk is out of bounds
+    if x <= 0 or y <= 0: return []
+    
+    # if the current chunk is the center
+    if center == Vector2(x, y):
+        res.append(center)
+
+    # if the current chunk is on the border
+    elif steepness_map[x][y].magnitude() <= body_of_water_threshold:
+        var tmp = second_derivative_map[x][y]
+        # if the sign of the 2nd derivative is the same as the direction from the center, this tile IS part of the body of water
+        if (tmp.x == 0 or tmp.x / abs(tmp.x) == direction_from_center.x / abs(direction_from_center.x)) and \
+            (tmp.y == 0 or tmp.y / abs(tmp.y) == direction_from_center.y / abs(direction_from_center.y)):
+            res.append(Vector2(x, y))
+
+    # otherwise determine whether the current chunk is 
+    else:
+        var tmp = steepness_map[x][y]
+        # if the sign of the 2nd derivative is the same as the direction from the center, this tile IS part of the body of water
+        if (tmp.x == 0 or tmp.x / abs(tmp.x) == direction_from_center.x / abs(direction_from_center.x)) and \
+            (tmp.y == 0 or tmp.y / abs(tmp.y) == direction_from_center.y / abs(direction_from_center.y)):
+            res.append(Vector2(x, y))
+
+    for dx in [1, 0, -1]:
+        for dy in [1, 0, -1]
+            # if the change is not 0, 0
+            if dx != 0 or dy != 0:
+                # recursive call on next chunk
+                res += fill_water_body(center, x + dx, y + dy, elevation_map, steepness_map, second_derivative_map)
+
+    return res
 
 
 func _phys_erosion():
     pass
+
 
 func _chem_erosion():
     # based off of moisture
 
     pass
 
+
 func _do_erosion():
     _chem_erosion()
     _phys_erosion()
 
+
 func _save():
     pass
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-
     if world_seed == null:
         randomize()
         world_seed = randi()
@@ -151,7 +240,6 @@ func _ready() -> void:
     var biome_count:Dictionary = {}
     for biome in BIOME_LIST.keys():
         biome_count[biome] = 0
-
 
     print("generating biomes")
     # first generate biome centers
@@ -176,7 +264,7 @@ func _ready() -> void:
     print("generating mountains")
     var elevation_map = generate_mountains()
 
-    var elevation_map = generate_water()
+    # var elevation_map = generate_water()
 
     var steepness_map = generate_steepness(elevation_map)
     # print("generating bodies of water")
